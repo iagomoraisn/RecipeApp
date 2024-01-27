e95e2169e9a2e3fd40eb455b7d6570fbf65a754e
iagomoraisn <iagojkd96@hotmail.com>
2024-01-27 12:12:12 -0300

    SpoonacularSetup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,11 +10,11 @@ app.use(cors());
 app.get("/api/recipes/search", async (req, res) => {
     const searchTerm = req.query.searchTerm as string;
     const page = parseInt(req.query.page as string);
-    const results = RecipeAPI.searchRecipes(searchTerm, page);
+    const results = await RecipeAPI.searchRecipes(searchTerm, page);
 
     return res.json(results);
-})
+});
 
-app.listen(5000, ()=> {
+app.listen(5000, () => {
     console.log("server running on localhost:5000");
 });
--- backend/src/recipe-api.ts
+++ backend/src/recipe-api.ts
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const apiKey = process.env.API_KEY;
 
-export const searchRecipes = async (searchTerm: string, page:number) => {
+export const searchRecipes = async (searchTerm: string, page: number) => {
     if(!apiKey) {
-        throw new Error("API Key not found")
+        throw new Error("API Key not found");
     }
 
     const url = new URL("https://api.spoonacular.com/recipes/complexSearch");
@@ -13,13 +14,17 @@ export const searchRecipes = async (searchTerm: string, page:number) => {
         number: "10",
         offset: (page * 10).toString()
     }
-    url.search = new URLSearchParams(queryParams).toString()
+        url.search = new URLSearchParams(queryParams).toString()
 
-    try {
-         const searchResponse = await fetch(url);
-        const resultsJson = await searchResponse.json();
-        return resultsJson;
-    } catch (error) {
-        console.log(error);
-    }
-};
+        try {
+            const searchResponse = await fetch(url);
+            if (searchResponse.headers.get('content-type')?.includes('application/json')) {
+                const resultsJson = await searchResponse.json();
+                return resultsJson;
+            } else {
+                console.error('API response was not JSON:', await searchResponse.text());
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    } 
