30a8e38dde11da4d9ac8c6067cc7a1a2dc83132b
iagomoraisn <iagojkd96@hotmail.com>
2024-02-05 08:42:34 -0300

    api.ts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,29 @@
 import './App.css';
+import { useState } from 'react';
+import * as api from './api';
 
 const App = () => {
-    return <div>Hello from recipe App!</div>;
+    const [searchTerm, setSearchTerm] = useState('burgers');
+    const [recipes, setRecipes] = useState([])
+
+    const handleSearchSubmit = async () => {
+        try {
+            const recipes = await api.searchRecipes(searchTerm, 1);
+            setRecipes(recipes);
+        } catch (e) {
+            console.log(e);
+
+        }
+    }
+
+    return <div>
+        {recipes.map(() => (
+            <div>
+                recipe image location: {recipes.image}
+                recipe title: {recipes.title}
+            </div>
+        ))}
+    </div>;
 };
 
 export default App;
