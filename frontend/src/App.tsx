c877a2848dd8f89d2a2d9712aaf10e2b21a1f602
iagomoraisn <iagojkd96@hotmail.com>
2024-02-07 09:40:26 -0300

    searchInputAndRecipeCardComponent

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,30 @@
 import './App.css';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 import * as api from './api';
 import { Recipe } from './types';
 
 const App = () => {
-    const [searchTerm, setSearchTerm] = useState('burgers');
+    const [searchTerm, setSearchTerm] = useState<string>('');
     const [recipes, setRecipes] = useState<Recipe[]>([]);
 
-    const handleSearchSubmit = async () => {
+    const handleSearchSubmit = async (event: FormEvent) => {
+        event.preventDefault();
         try {
             const recipes = await api.searchRecipes(searchTerm, 1);
-            setRecipes(recipes);
+            setRecipes(recipes.results);
         } catch (e) {
             console.log(e);
 
         }
-    }
+    };
 
     return (
     <div>
-        <form onSubmit={ () => handleSearchSubmit()}>
+        <form onSubmit={(event) => handleSearchSubmit(event)}>
+            <input type="text" required placeholder='Enter a search term ...'
+            value={searchTerm}
+            onChange={(event) => setSearchTerm(event.target.value)}
+            ></input>
         <button type="submit">Submit</button>
         </form>
         
