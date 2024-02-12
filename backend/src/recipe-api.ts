585503e1418f2bafcaeff82c6c36f641fd035450
iagomoraisn <iagojkd96@hotmail.com>
2024-02-12 19:39:05 -0300

    SummaryModal&EndPoint

--- backend/src/recipe-api.ts
+++ backend/src/recipe-api.ts
@@ -12,8 +12,8 @@ export const searchRecipes = async (searchTerm: string, page: number) => {
         apiKey,
         query: searchTerm,
         number: "10",
-        offset: (page * 10).toString()
-    }
+        offset: (page * 10).toString(),
+    };
         url.search = new URLSearchParams(queryParams).toString()
 
         try {
@@ -24,4 +24,18 @@ export const searchRecipes = async (searchTerm: string, page: number) => {
         } catch (error) {
             console.log(error);
         }
-    } 
+    };
+
+    export const getRecipeSummary = async (recipeId:string) => {
+        if(!apiKey) {
+            throw new Error("API Key not found");
+        }
+        const url = new URL(`https://api.spoonacular.com/recipes/${recipeId}/summary`);
+        const params = {
+            apiKey: apiKey
+        }
+        url.search = new URLSearchParams(params).toString();
+        const response = await fetch(url);
+        const json = await response.json();
+        return json;
+    };
