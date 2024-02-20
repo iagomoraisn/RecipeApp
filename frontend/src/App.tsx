90b9c0ae1e7648f63a049016589398ad1f12525e
iagomoraisn <iagojkd96@hotmail.com>
2024-02-20 08:49:50 -0300

    RecipeCard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,12 @@ import { FormEvent, useRef, useState } from 'react';
 import * as api from './api';
 import { Recipe } from './types';
 import RecipeCard from './components/RecipeCard';
+import RecipeModal from './components/RecipeModal';
 
 const App = () => {
     const [searchTerm, setSearchTerm] = useState<string>('');
     const [recipes, setRecipes] = useState<Recipe[]>([]);
+    const [selectedRecipe, setSelectedRecipe] = useState<Recipe | undefined>(undefined);
     const pageNumber = useRef(1);
 
     const handleSearchSubmit = async (event: FormEvent) => {
@@ -44,12 +46,13 @@ const App = () => {
         </form>
         
         {recipes.map((recipe) => (
-            <RecipeCard recipe={recipe}/>
+            <RecipeCard recipe={recipe} onClick={() => setSelectedRecipe(recipe)} />
             ))}
             <button 
             className="view-more-button"
             onClick={handleViewMoreClick}
             >View More</button>
+            {selectedRecipe ? <RecipeModal /> : null}
             </div>
     );
 };
