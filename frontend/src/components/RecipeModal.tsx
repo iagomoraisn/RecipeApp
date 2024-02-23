6fbd626b223dc9db1866c3011ac8baf67d57be5c
iagomoraisn <iagojkd96@hotmail.com>
2024-02-23 11:39:37 -0300

    newRoutes

--- frontend/src/components/RecipeModal.tsx
+++ frontend/src/components/RecipeModal.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
+import { RecipeSummary } from '../types';
+
 const RecipeModal = () => {
+
+    const [recipeSummary, setRecipeSummary] = useState<RecipeSummary>();
+
     return (
         <>
         <div className = "overlay"></div>
         <div className = "modal">
         <div className = "modal-content">
             <div className = "modal-header">
-                <h2>RECIPE TITLE</h2>
+                <h2>{recipeSummary?.id}</h2>
                 <span className= "close-btn">
                     &times;
                 </span>
